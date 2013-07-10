248668b074fc6e0dc03ab042638dac722d0e4053
cgkaczenski <kzpulaski@gmail.com>
2013-07-10 12:23:10 +0000

    Update

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync("index.html")));
+  var fs = require ('fs');
+  var content = fs.readFileSync('index.html');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
